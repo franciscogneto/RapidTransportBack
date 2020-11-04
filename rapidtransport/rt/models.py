0f05ac7ba93fdac94106a6b4d25975763eabdf0f
franciscogneto <francisco_gneto@hotmail.com>
2020-11-04 13:45:19 -0300

    alteração no banco de dados

--- rapidtransport/rt/models.py
+++ rapidtransport/rt/models.py
@@ -7,6 +7,8 @@ class Usuario(AbstractUser):
     is_funcionario = models.BooleanField(default=False)
     is_empresa = models.BooleanField(default=False)
 
+    class Meta:
+        db_table = "usuarios"
     
 class Empresa(models.Model):
     usuario = models.OneToOneField(Usuario,on_delete=models.CASCADE,primary_key=True)
@@ -24,6 +26,11 @@ class Empresa(models.Model):
 
 
 class Veiculo(models.Model):
+    STATUS = (
+        (1,'DISPONÍVEL'),
+        (2,'AUSENTE'),
+        (3,'INOPERANTE')
+    )
     id = models.AutoField(primary_key=True)
     modelo = models.CharField(max_length=50)
     color = models.CharField(max_length=25)
@@ -31,6 +38,7 @@ class Veiculo(models.Model):
     kilometragem_inicial = models.PositiveIntegerField()
     kilometragem_revisao = models.PositiveSmallIntegerField()
     empresa = models.ForeignKey(Empresa,on_delete=models.CASCADE)
+    status = models.PositiveSmallIntegerField(choices=STATUS,default=1)
     
     #Como será mostrado este model no admin por exemplo quando criado um novo objeto.
     def __str__(self):
@@ -47,6 +55,12 @@ class Funcionario(models.Model):
         ('D', 'ONIBUS'),
         ('E', 'CARRETA'),
     )
+    STATUS = (
+        (1,'DISPONÍVEL'),
+        (2,'AUSENTE'),
+        (3,'INOPERANTE')
+    )
+
     usuario = models.OneToOneField(Usuario,on_delete=models.CASCADE,primary_key=True)
     empresa = models.ForeignKey(Empresa,on_delete=models.CASCADE)
     nome = models.CharField(max_length=100)
@@ -54,6 +68,7 @@ class Funcionario(models.Model):
     data_aniversario = models.DateField()
     tipo_carteira = models.CharField(max_length=1,choices=TIPO_CARTA)
     celular = models.CharField(max_length=18)
+    status = models.PositiveSmallIntegerField(choices=STATUS,default=1)
 
     def __str__(self):
         return self.nome
@@ -61,3 +76,17 @@ class Funcionario(models.Model):
         db_table = "funcionarios"
 
 
+class Item(models.Model):
+    TIPO_GRAU = (
+        (1,'LEVE'),
+        (2,'MODERADO'),
+        (3,'URGENTE'),
+    )
+    id = models.AutoField(primary_key=True)
+    #id_relatório
+    grau_emergencia = models.PositiveSmallIntegerField(choices=TIPO_GRAU)
+    descricao = models.CharField(max_length=250)
+    ok = models.BooleanField()
+
+    class Meta:
+        db_table = "item"
