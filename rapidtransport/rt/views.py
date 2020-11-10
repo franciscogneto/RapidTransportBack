d04f2a07239d3d8b6cf49f08c1203d162f63b35d
franciscogneto <francisco_gneto@hotmail.com>
2020-11-10 15:31:25 -0300

    adicionado view para o login; view para pegar todos os funcionários e adicionar um novo; view para deletar e alterar um funcionário em específico

--- rapidtransport/rt/views.py
+++ rapidtransport/rt/views.py
@@ -6,13 +6,54 @@ from rest_framework.permissions import IsAuthenticated, AllowAny
 from .models import Empresa,Veiculo,Funcionario,Usuario
 from .serializers import EmpresaSerializer,VeiculoSerializer,FuncionarioSerializer,UsuarioSerializer
 from django.core import serializers
-from django.contrib.auth.hashers import make_password
+from django.contrib.auth.hashers import make_password,check_password
 from .permissions.permissions import IsEmpresaUser,IsFuncionarioUser
-from .regularizadores.regularizadorVeiculo import RegularizadorVeiculo
+from .regularizadores.auxiliar import GeraSenhaUsuario,RegularizadorVeiculo,RegularizaFuncionario
+from django.shortcuts import redirect
+from rest_framework_simplejwt.tokens import RefreshToken 
 # Create your views here.
 
+
+class LoginView(APIView):
+    permission_classes = (AllowAny,)
+
+    def post(self,request):
+        username = request.data['username']
+        password = request.data['password']
+        usuario = Usuario.objects.get(username=username)
+        if(usuario.check_password(password)):
+            jwt = LoginView.token_para_usuario(LoginView,usuario)
+            if(usuario.is_empresa):
+                resposta = {
+                    'refresh': str(jwt['refresh']),
+                    'access': str(jwt['access']),
+                    'isFuncionario': 0,
+                    'isEmpresa':1,
+                }
+                return Response(status=status.HTTP_200_OK,data=resposta)
+            elif(usuario.is_funcionario):
+                resposta = {
+                    'refresh': str(jwt['refresh']),
+                    'access': str(jwt['token']),
+                    'isFuncionario': 1,
+                    'isEmpresa':0,
+                }
+                return Response(status=status.HTTP_200_OK,data=resposta)
+            else:
+                return Response(status=status.HTTP_400_BAD_REQUEST,data="Erro no Login, refaça a operação")#formatação incorreta
+        else:
+            return Response(status=status.HTTP_401_UNAUTHORIZED,data="usuário ou senha incorreto") 
+
+    def token_para_usuario(self,usuario):
+        refresh = RefreshToken.for_user(usuario)
+        
+        return{
+            'refresh': str(refresh),
+            'access': str(refresh.access_token),
+        }
+
 class EmpresaAPIView(APIView):
-    permission_classes = (IsEmpresaUser,)
+    permission_classes = (AllowAny,)
 
     def get(self,request):
         empresas = Empresa.objects.all()
@@ -20,9 +61,7 @@ class EmpresaAPIView(APIView):
         return Response(serializer.data)
 
     def put(self,request):
-        
-        serializer = EmpresaSerializer(data=request.data)
-        
+        serializer = EmpresaSerializer(data=request.data)      
         if(serializer.is_valid()):
             
             try:
@@ -48,8 +87,8 @@ class UsuarioAPIView(APIView):
 
     def get(self,request):
         usuarios = Usuario.objects.all()
-        serializer = UsuarioSerializer(data=usuarios,many=True)
-        return Response(serializer.data)
+        serializer = UsuarioSerializer(usuarios,many=True)
+        return Response(data=serializer.data)
 
 ############################################# final dos teste e começo das views
 
@@ -113,22 +152,78 @@ class VeiculoDetalheApiView(APIView):
         else:
             return Response(status=status.HTTP_404_NOT_FOUND, data="veículo não encontrado")
 
-class FuncionarioAPIView(APIView):
+class FuncionarioListaAPIView(APIView):
 
     permission_classes = (AllowAny,)
 
-    def get_object(self,request,pk):
-        funcionario = Funcionario.objects.get(id=pk)
-        serializer = FuncionarioSerializer(funcionario)
-        return Response(status=status.HTTP_200_OK,data=serializer.data)
 
     def get(self,request):
         funcionario = Funcionario.objects.all()
         serializer = FuncionarioSerializer(funcionario,many=True)
         return Response(serializer.data)
 
+    def post(self,request):
 
-class FuncionarioByIdView(generics.RetrieveUpdateDestroyAPIView):#opção de alterar,pegar e deletar
-    queryset = Funcionario.objects.all()
-    serializer_class = FuncionarioSerializer
+        if(not(Funcionario.objects.filter(cpf=request.data['cpf']).exists())):
+            print(request.data['cpf'])
+            if(RegularizaFuncionario.regulariza_funcionario(RegularizaFuncionario,request.data['cpf'],request.data['nome'],request.data['celular'],request.data['data_aniversario'],request.data['data_admissao'])):
+                if(not(Empresa.objects.filter(usuario=request.data['empresa']).exists())):
+                    return Response(status=status.HTTP_400_BAD_REQUEST, data="não foi possível criar o funcionários, check os dados")
+                empresa_aux = Empresa.objects.get(usuario=request.data['empresa'])
+                senha = GeraSenhaUsuario.gera_senha(GeraSenhaUsuario)
+                usuario = Usuario.objects.create(username=request.data['cpf'],password=make_password(senha))
+                funcionario  = Funcionario.objects.create(usuario=usuario,
+                nome=request.data['nome'],
+                data_admissao=request.data['data_admissao'],
+                data_aniversario=request.data['data_aniversario'],
+                tipo_carteira=request.data['tipo_carteira'],
+                celular=request.data['celular'],
+                cpf= request.data['cpf'],
+                empresa= empresa_aux
+                )
+                data = {
+                    'usuario' : request.data['cpf'],
+                    'senha' : senha
+                }
+                return Response(status=status.HTTP_201_CREATED,data=data)
+            else:
+                return Response(status=status.HTTP_400_BAD_REQUEST, data="não foi possível criar o funcionários, check os dados")
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST,data="cpf já existente")
 
+class FuncionarioDetalheAPIView(APIView):
+    permission_classes = (AllowAny,)
+    def get(self,request,id):
+        if(Funcionario.objects.filter(usuario=id)):
+            funcionario = Funcionario.objects.get(usuario=id)
+            serializer = FuncionarioSerializer(funcionario)
+            return Response(status=status.HTTP_200_OK,data=serializer.data)
+        return Response(status=status.HTTP_400_BAD_REQUEST,data="funcionário inexistente")
+    
+    def put(self,request,id):
+        if(Funcionario.objects.filter(usuario=id).exists()):
+            if(RegularizaFuncionario.regulariza_funcionario(RegularizaFuncionario,request.data['cpf'],request.data['nome'],request.data['celular'],request.data['data_aniversario'],request.data['data_admissao'])):
+                usuarios = Usuario.objects.filter(username=request.data['cpf'])
+                for usuario in usuarios:  
+                    if(usuario.username == request.data['cpf'] and usuario.id != id):
+                        return Response(status=status.HTTP_400_BAD_REQUEST,data='não foi possível a atualização do funcionário, porfavor check os dados inseridos')
+                funcionario = Funcionario.objects.get(usuario=usuarios[0])
+                serializer = FuncionarioSerializer(funcionario, data=request.data)
+                if(serializer.is_valid()):
+                    serializer.save()
+                    return Response(status=status.HTTP_200_OK,data=serializer.data)
+                else:
+                    return Response(serializer.errors)
+                
+        return Response(status=status.HTTP_400_BAD_REQUEST,data='não foi possível a atualização do funcionário, porfavor check os dados inseridos')
+    
+    def delete(self,request,id):
+        if(Funcionario.objects.filter(usuario=id).exists()):
+            usuario = Usuario.objects.get(id=id)
+            funcionario = Funcionario.objects.get(usuario=usuario)
+            mensagem = "excluído com sucesso funcionário: nome:"+funcionario.nome+" cpf: "+funcionario.cpf
+            funcionario.delete()
+            usuario.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT , data=mensagem)
+        else:
+            return Response(status=status.HTTP_404_NOT_FOUND, data="funcionário não encontrado")
