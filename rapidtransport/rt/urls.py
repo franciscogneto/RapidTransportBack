e2c7c5c2a7b08004cc560462156fc05796fc7fd0
franciscogneto <francisco_gneto@hotmail.com>
2020-11-10 15:07:50 -0300

    importado view para o login e views para funcionário para realização de requisições & adicionado as urls dos mesmos

--- rapidtransport/rt/urls.py
+++ rapidtransport/rt/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
-from .views import EmpresaAPIView,FuncionarioAPIView,FuncionarioByIdView,UsuarioAPIView,VeiculoListaAPIView,VeiculoDetalheApiView
-
+from .views import EmpresaAPIView,FuncionarioListaAPIView,FuncionarioDetalheAPIView,UsuarioAPIView,VeiculoListaAPIView,VeiculoDetalheApiView
+from .views import LoginView
 
 urlpatterns = [
+    path('login/',LoginView.as_view(), name='login'),
     path('veiculos/',VeiculoListaAPIView.as_view(), name = 'veiculos'),
     path('veiculos/<int:id>/',VeiculoDetalheApiView.as_view(),name = 'veiculo'),
-    path('funcionarios/',FuncionarioAPIView.as_view(), name = 'funcionarios'),
-    path('funcionarios/<int:id>/',FuncionarioAPIView.as_view()),
+    path('funcionarios/',FuncionarioListaAPIView.as_view(), name = 'funcionarios'),
+    path('funcionarios/<int:id>/',FuncionarioDetalheAPIView.as_view()),
     path('empresa/',EmpresaAPIView.as_view(), name = 'empresa'),
     path('usuarios/',UsuarioAPIView.as_view(), name = 'usuarios')
 ]
