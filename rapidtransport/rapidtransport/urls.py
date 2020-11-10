f9f734e57cd9b4b8afbfb0df323c73cd0fcdbc04
franciscogneto <francisco_gneto@hotmail.com>
2020-11-10 15:04:14 -0300

    exclusão de imports desnecessários

--- rapidtransport/rapidtransport/urls.py
+++ rapidtransport/rapidtransport/urls.py
@@ -15,8 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from rest_framework.authtoken import views
-from django.contrib.auth.views import LoginView
 from rest_framework_simplejwt import views as jwt_views
 
 urlpatterns = [
@@ -25,4 +23,5 @@ urlpatterns = [
     path('auth/', include('rest_framework.urls')),
     path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+
 ]
