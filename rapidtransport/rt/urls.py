87030d4d47af7ca87370bb2c129382a12114880a
franciscogneto <francisco_gneto@hotmail.com>
2020-11-22 16:42:57 -0300

    adição das novas urls para as novas funcionalidades do sistema

--- rapidtransport/rt/urls.py
+++ rapidtransport/rt/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
-from .views import EmpresaAPIView,FuncionarioListaAPIView,FuncionarioDetalheAPIView,UsuarioAPIView,VeiculoListaAPIView,VeiculoDetalheApiView,RelatorioListaAPIView,ItemAPIView,testeItemApiView,RelatorioDetalheAPIView
+from .views import EmpresaAPIView,FuncionarioListaAPIView,FuncionarioDetalheAPIView
+from .views import UsuarioAPIView,VeiculoListaAPIView,VeiculoDetalheApiView
+from .views import RelatorioListaAPIView,ItemAPIView,testeItemApiView,RelatorioDetalheAPIView
+from .views import RevisaoListaAPIView,RevisaoDetalheAPIView,ViagemListaAPIView,ViagemDetalheAPIView
 from .views import LoginView
 
 urlpatterns = [
@@ -13,6 +16,10 @@ urlpatterns = [
     path('relatorios/',RelatorioListaAPIView.as_view(), name = 'relatorio'),
     path('relatorios/<int:id>/',RelatorioDetalheAPIView.as_view(), name = 'exclui_relatorio'),
     path('itens/',ItemAPIView.as_view(),name = 'itens'),
-    path('item/<int:id_item>/',testeItemApiView.as_view(),name = 'exclui_item')
+    path('item/<int:id_item>/',testeItemApiView.as_view(),name = 'exclui_item'),
+    path('revisoes/<int:id_veiculo>/', RevisaoListaAPIView.as_view(), name = 'revisoes'),
+    path('revisao/<int:id_revisao>/',RevisaoDetalheAPIView.as_view(),name = 'revisao'),
+    path('viagens/',ViagemListaAPIView.as_view(), name = 'viagens'),
+    path('viagens/<int:id>/', ViagemDetalheAPIView.as_view(), name = 'viagem'),
 ]
 
