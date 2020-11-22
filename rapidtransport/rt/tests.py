0f1cb0e87f225d04e46e82de597829b4f545d5a5
franciscogneto <francisco_gneto@hotmail.com>
2020-11-22 15:25:05 -0300

    aplicação de testes

--- rapidtransport/rt/tests.py
+++ rapidtransport/rt/tests.py
@@ -1,3 +1,33 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from .regularizadores.auxiliar import GeraSenhaUsuario,RegularizaViagem
 
-# Create your tests here.
+
+class TesteRegularizadores(TestCase):
+
+    def teste_senhas_geradas(self):#analisando se senha tem o formato correto
+        teste = False
+        qtd_letra = 0
+        qtd_numero = 0
+        senha = GeraSenhaUsuario().gera_senha()
+        for letra in senha:
+            if(letra >= '0' and letra <= '9'):
+                qtd_numero += 1
+            elif letra >= 'a' and letra <= 'z':
+                qtd_letra += 1
+        if(qtd_numero != 4 and qtd_letra != 10):
+            teste = True
+        self.assertFalse(teste, 'Senha padronizada de forma incorreta')
+        self.assertTrue(teste, 'Senha padronizada de forma correta')
+
+
+    def teste_login(self):
+        teste = False
+        c = Client()
+        objeto = {
+            'username': '6542476000137',
+            'password': '1122'
+        }
+        resposta = c.post('http://127.0.0.1:8000/api/login/',data=objeto)      
+        self.assertEquals(200,resposta.status_code,'logado com sucesso')
+        self.assertEquals(401,resposta.status_code,'não foi possível')
+        
