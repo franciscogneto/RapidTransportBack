807602acda36de9cebbdb898ac2aec83ea4bcb44
franciscogneto <francisco_gneto@hotmail.com>
2020-11-22 16:28:31 -0300

    adiconado nova permissão

--- rapidtransport/rt/permissions/permissions.py
+++ rapidtransport/rt/permissions/permissions.py
@@ -16,3 +16,9 @@ class IsEmpresaUser(BasePermission):
 
     def has_permission(self, request, view):
         return bool(request.user and request.user.is_empresa)
+
+
+class IsSuperUser(BasePermission):
+
+    def has_permission(self, request, view):
+        return bool(request.user and request.user.is_superuser)
